d22f00d0f0c245ed0e816cae7bcd87c58e0c31ad
Ihor Fito <terysif@gmail.com>
2017-11-15 15:13:20 +0200

    add invocations and functions API

---
+++ src/api/functions/functions.js
@@ -0,0 +1,66 @@
+'use strict';
+const FunctionModel = require('../../shared/model/function');
+const _ = require('lodash');
+const passport = require('./../passport/passport')
+
+module.exports.list = (event, context, callback) => {
+  return passport.checkAuth(event.headers.Authorization)
+    .then((user) => {
+      return FunctionModel.getAll()
+    })
+    .then((result) => {
+      return {
+        statusCode: 200,
+        error: null,
+        result: result
+      }
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        error: err.message,
+        result: null
+      }
+    })
+    .finally((object) => {
+      const response = {
+        statusCode: object.statusCode,
+        body: JSON.stringify({
+          error: object.error,
+          result: object.result
+        }),
+      };
+      callback(null, response);
+    })
+};
+
+module.exports.get = (event, context, callback) => {
+  return passport.checkAuth(event.headers.Authorization)
+    .then((user) => {
+      return FunctionModel.getById(event.pathParameters.id)
+    })
+    .then((result) => {
+      return {
+        statusCode: 200,
+        error: null,
+        result: result
+      }
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        error: err.message,
+        result: null
+      }
+    })
+    .finally((object) => {
+      const response = {
+        statusCode: object.statusCode,
+        body: JSON.stringify({
+          error: object.error,
+          result: object.result
+        }),
+      };
+      callback(null, response);
+    })
+};
---
+++ src/api/invocations/functions.js
@@ -0,0 +1,66 @@
+'use strict';
+const InvocationModel = require('../../shared/model/invocation');
+const _ = require('lodash');
+const passport = require('./../passport/passport')
+
+module.exports.list = (event, context, callback) => {
+  return passport.checkAuth(event.headers.Authorization)
+    .then((user) => {
+      return InvocationModel.getAll()
+    })
+    .then((result) => {
+      return {
+        statusCode: 200,
+        error: null,
+        result: result
+      }
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        error: err.message,
+        result: null
+      }
+    })
+    .finally((object) => {
+      const response = {
+        statusCode: object.statusCode,
+        body: JSON.stringify({
+          error: object.error,
+          result: object.result
+        }),
+      };
+      callback(null, response);
+    })
+};
+
+module.exports.get = (event, context, callback) => {
+  return passport.checkAuth(event.headers.Authorization)
+    .then((user) => {
+      return InvocationModel.getById(event.pathParameters.id)
+    })
+    .then((result) => {
+      return {
+        statusCode: 200,
+        error: null,
+        result: result
+      }
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        error: err.message,
+        result: null
+      }
+    })
+    .finally((object) => {
+      const response = {
+        statusCode: object.statusCode,
+        body: JSON.stringify({
+          error: object.error,
+          result: object.result
+        }),
+      };
+      callback(null, response);
+    })
+};
