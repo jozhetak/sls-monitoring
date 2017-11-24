db72d095bdcf88ae57e7629f5ee70dea635c30b4
Ihor Fito <terysif@gmail.com>
2017-11-24 12:37:58 +0200

    mockup account users

--- src/api/accounts/accounts.js
+++ src/api/accounts/accounts.js
@@ -219,3 +219,63 @@ module.exports.delete = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 }
+
+module.exports.inviteUsers = (event, context, callback) => {
+  const response = {
+    statusCode: 501,
+    body: JSON.stringify({
+      message: 'NOT_IMPLEMENTED',
+      input: event
+    })
+  }
+
+  callback(null, response)
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+}
+
+module.exports.getAccountUsers = (event, context, callback) => {
+  const response = {
+    statusCode: 501,
+    body: JSON.stringify({
+      message: 'NOT_IMPLEMENTED',
+      input: event
+    })
+  }
+
+  callback(null, response)
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+}
+
+module.exports.updateAccountUser = (event, context, callback) => {
+  const response = {
+    statusCode: 501,
+    body: JSON.stringify({
+      message: 'NOT_IMPLEMENTED',
+      input: event
+    })
+  }
+
+  callback(null, response)
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+}
+
+module.exports.deleteAccountUser = (event, context, callback) => {
+  const response = {
+    statusCode: 501,
+    body: JSON.stringify({
+      message: 'NOT_IMPLEMENTED',
+      input: event
+    })
+  }
+
+  callback(null, response)
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+}
