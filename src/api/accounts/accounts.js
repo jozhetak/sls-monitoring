abdff49b165f23c6e246cd1c734af4d816aa44c5
Ihor Fito <terysif@gmail.com>
2017-11-27 11:44:18 +0200

    delete Account API

--- src/api/accounts/accounts.js
+++ src/api/accounts/accounts.js
@@ -130,18 +130,28 @@ module.exports.update = (event, context, callback) => {
 }
 
 module.exports.delete = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
+  console.log('event: ', event)
+  return passport.checkAuth(event.headers.Authorization)
+    .then((decoded) => {
+      return AccountModel.getById(event.pathParameters.id)
+        .then((account) => {
+          if (account._user !== decoded.user._id) {
+            throw Error('User has no permission')
+          }
+          return account
+        })
+        .catch((err) => {
+          throw err
+        })
     })
-  }
-
-  callback(null, response)
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    .then((account) => {
+      return AccountModel.update(account._id, {
+        isActive: false
+      })
+    })
+    .then(responses.deleted)
+    .catch(responses.error)
+    .then(response => callback(null, response))
 }
 
 module.exports.inviteUsers = (event, context, callback) => {
