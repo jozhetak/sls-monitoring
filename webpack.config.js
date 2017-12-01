bb8863c9af1bf3d5e880c63b2d0a387f09ab3ce7
Ihor Fito <terysif@gmail.com>
2017-12-01 13:34:56 +0200

    deploy using webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  }
+}
