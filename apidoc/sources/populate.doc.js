2e613bf70da4329263668ec813629768c62a613f
DmytroKucheryavenko <dmytro.kucheryavenko@techmagic.co>
2018-02-13 20:25:41 +0200

    update apidoc

--- apidoc/sources/populate.doc.js
+++ apidoc/sources/populate.doc.js
@@ -1,6 +1,6 @@
 
 /**
- * @api {post} accounts/:id/functions Populate functions & invocations
+ * @api {post} /accounts/:id/functions Populate functions & invocations
  * @apiName Populate
  * @apiVersion 1.0.0
  * @apiDescription Populate test data in database.
