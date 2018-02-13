93513d6abbf1a37cc64b24a5976d2e45934b1621
DmytroKucheryavenko <dmytro.kucheryavenko@techmagic.co>
2018-02-13 19:35:48 +0200

    update apidoc

--- apidoc/sources/populate.doc.js
+++ apidoc/sources/populate.doc.js
@@ -10,15 +10,19 @@
  *     {
  *       "id": "6beaa4e0-e663-11e7-a4b1-53f92960e92c"
  *     }
- * @apiParam  {Number} days The difference between current date and minimum possible date for test data generation. If no specified, the default value is 365 days
- * @apiParam  {Number} errors The coefficient of errors for invocations from 0 to 1. 0.5 means that approximately every second invocation will fail. If no specified, the default value is 0.5
- * @apiParam  {String} invocations_min The minimum possible value of  function invocations. If no specified, the default value is 5
- * @apiParam  {String} invocations_max The maximum possible value of  function invocations. If no specified, the default value is 10
- * @apiParam  {String} functions_count The count of functions to populate.
- * @apiParamExample {String} Request Query Example
- {
-    days=31&errors=0.6&invocations_min=1&invocations_max=5&functions_count=2
- }
+ * @apiParam  {Number} daysInPast The difference between current date and minimum possible date for test data generation. If no specified, the default value is 1 day
+ * @apiParam  {Number} errorsCoefficient The coefficient of errors for invocations from 0 to 1. 0.5 means that approximately every second invocation will fail. If no specified, the default value is 0.5
+ * @apiParam  {String} invocationsMin The minimum possible value of  function invocations. If no specified, the default value is 5
+ * @apiParam  {String} invocationsMax The maximum possible value of  function invocations. If no specified, the default value is 10
+ * @apiParam  {String} functionsCount The count of functions to populate. If no specified, the default value is 1
+ * @apiBodyExample {String} Request Body Example
+   {
+    "daysInPast": 31,
+    "errorsCoefficient": 0.8,
+    "invocationsMin":1,
+    "invocationsMax":15,
+    "functionsCount":25
+  }
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 201 Created
 */
