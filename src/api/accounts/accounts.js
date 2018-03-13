a7936ccdf7de7daa4f6c4cfe0b05e3baae2d7420
Yuriy Khrystuk <22624868+khrystuk@users.noreply.github.com>
2018-03-13 12:46:46 +0200

    fixed changing user roles

--- src/api/accounts/accounts.js
+++ src/api/accounts/accounts.js
@@ -292,8 +292,8 @@ module.exports.updateAccountUser = (event, context, callback) => {
     })
     .then(([id, accountUsers]) => {
       let accountUsersIds = accountUsers.map(user => user._user)
-      if (!(accountUsersIds.includes(id) &&
-          accountUsers.find(user => user._user === id).isAdmin)) {
+      if (!(accountUsersIds.includes(id) && accountUsers.find(user => user._user === id).isAdmin) ||
+        (accountUsers.filter(user => user._user !== id && user.isAdmin).length === 0)) {
         throw errors.forbidden()
       }
     })
